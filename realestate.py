f302c69bd01a6c2ecfb891bb90bbb6c85474f1fd
Nan Li <linan.lqq0@gmail.com>
2018-08-14 23:57:41 +1000

    re-write code by using class

--- realestate.py
+++ realestate.py
@@ -15,7 +15,7 @@ from mysql.connector import errorcode
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-
+# function to read postcode from excel file
 def readPostcode(file_name):
     os.chdir(os.path.dirname(__file__))
     postcode = pd.read_excel(file_name).sort_values(by=['Postcode'])
@@ -23,12 +23,102 @@ def readPostcode(file_name):
     postcode = postcode[:,0]
     return postcode
 
+# function to create valid url
 def createUrl(postcode, page_no):
     url_pre = 'https://www.realestate.com.au/sold/in-'
     url = '{}{}/list-{}?includeSurrounding=false'.format(url_pre, postcode, page_no)
     return url
 
- 
+# class to send request to web server
+class Request:
+    def __init__(self, url):
+        self.url = url
+        self.text = requests.get(self.url).text
+        self.soup = BeautifulSoup(self.text, 'html.parser')
+
+# class to extract infos from list page
+class ExtractListPage(Request):
+    def getNextPage(self):
+        nextpage = self.soup.find('a', attrs={'title':'Go to Next Page'})
+        return nextpage
+    
+    def getHouseLink(self):
+        houselinks = self.soup.findAll('a', attrs={'href': re.compile(r'/sold/property-\S+-vic-\S+[0-9]')})
+        return houselinks
+
+# class to get house infos from house page
+class House(Request):
+    def getHouseID(self):
+        house_id = self.soup.find('span', attrs={'class':'listing-metrics__property-id'})
+        if house_id is not None:
+            house_id = int(re.search(r'[0-9]+', house_id.text).group())
+        return house_id
+
+    def getAddress(self):
+        address = self.soup.find('span', attrs={'class':'property-info-address__street'}).text
+        return address
+
+    def getSuburb(self):
+        suburb_temp = self.soup.find('span', attrs={'class':'property-info-address__suburb'}).text
+        suburb = re.search(r'[a-z|A-Z|\s]+,', suburb_temp).group()[:-1]
+        post_code = re.search(r'[0-9]+', suburb_temp).group()
+        return suburb, post_code
+
+    def getHouseType(self):
+        housetype = self.soup.find('span', attrs={'class':'property-info__property-type'}).text
+        return housetype
+
+    def getFeatures(self):
+        bedroom = 0
+        bathroom = 0
+        parking = 0
+        feature_str = ''
+        for feature in self.soup.findAll('li', attrs={'class':'general-features__feature'}):
+            feature_str = feature_str + feature.get('aria-label')
+        bed = re.search(r'[0-9]+\sbedroom', feature_str)
+        if bed is not None:
+            bedroom = int(re.search(r'[0-9]+', bed.group()).group())
+        bath = re.search(r'[0-9]+\sbathroom', feature_str)
+        if bath is not None:
+            bathroom = int(re.search(r'[0-9]+', bath.group()).group())
+        park = re.search(r'[0-9]+\sparking space', feature_str)
+        if park is not None:
+            parking = int(re.search(r'[0-9]+', park.group()).group())
+        return bedroom, bathroom, parking
+
+    def getSoldPrice(self):
+        soldprice = None
+        soldprice_str = self.soup.find('span', attrs={'class':'property-price property-info__price'}).text
+        price = re.search(r'[0-9,]+', soldprice_str)
+        if price is not None:
+            soldprice = int(price.group().replace(',', ''))
+        return soldprice
+
+    def getSoldDate(self):
+        solddate = self.soup.find('p', attrs={'class':'property-info__secondary-content'})
+        if solddate is not None:
+            solddate = solddate.text[8:]
+            try:
+                solddate = datetime.strptime(solddate, '%d %b %Y')
+            except:
+                solddate = None
+        return solddate
+    
+    def getAgency(self):
+        agency = self.soup.find('p', attrs={'class':'agency-info__name'})
+        if agency is not None:
+            agency = agency.text
+        return agency
+
+    def getLocation(self):
+        latitude = re.search(r'"latitude":-[0-9|.]+', self.text)
+        if latitude is not None:
+            latitude = -float(re.search(r'[0-9|.]+', latitude.group()).group())
+        longitude = re.search(r'"longitude":[0-9|.]+', self.text)
+        if longitude is not None:
+            longitude = float(re.search(r'[0-9|.]+', longitude.group()).group())
+        return latitude, longitude
+
 if __name__ == '__main__':
     # connect mySQL database
     try:
@@ -44,9 +134,9 @@ if __name__ == '__main__':
         exit()
     # create table if not exist
     try:
-        sql = """ CREATE TABLE house_info_main (
+        sql = """ CREATE TABLE victoria_test (
             id INTEGER AUTO_INCREMENT PRIMARY KEY,
-            house_id INTEGER,
+            house_id INTEGER UNIQUE,
             address VARCHAR(100),
             suburb VARCHAR(50),
             postcode INTEGER,
@@ -59,11 +149,15 @@ if __name__ == '__main__':
             agency VARCHAR(100),
             latitude DOUBLE PRECISION,
             longitude DOUBLE PRECISION,
-            link VARCHAR(200) ) """
+            link VARCHAR(200),
+            time TIMESTAMP ) """
         cursor = con.cursor()
         cursor.execute(sql)
+        print('create table successfully!')
     except:
+        print('table already exist!')
         pass
+
     # scrape data according to postcode
     house_count = 0
     file_name = 'Victoria.xlsx'
@@ -75,76 +169,40 @@ if __name__ == '__main__':
             url = createUrl(postcode, page)
             while True:
                 try:
-                    list_page = requests.get(url).text
+                    list_page = ExtractListPage(url)
                     break
                 except:
                     time.sleep(5)
                     continue
-            list_soup = BeautifulSoup(list_page, 'html.parser')
-            nextpage = list_soup.find('a', attrs={'title':'Go to Next Page'})
-            for house_link in list_soup.findAll('a', attrs={'href': re.compile
-                                (r'/sold/property-\S+-vic-\S+[0-9]')}):
+            nextpage = list_page.getNextPage()
+            houselinks = list_page.getHouseLink()
+            for house_link in houselinks:
                 house_url = 'https://www.realestate.com.au' + house_link.get('href')
                 while True:
                     try:
-                        house_page = requests.get(house_url).text
+                        house = House(house_url)
                         break
                     except:
                         time.sleep(5)
                         continue
-                house_soup = BeautifulSoup(house_page, 'html.parser')
-                house_id = house_soup.find('span', attrs={'class':'listing-metrics__property-id'})
-                if house_id is not None:
-                    house_id = int(re.search(r'[0-9]+', house_id.text).group())
                 try:
-                    address = house_soup.find('span', attrs={'class':'property-info-address__street'}).text
-                    suburb_temp = house_soup.find('span', attrs={'class':'property-info-address__suburb'}).text
-                    suburb = re.search(r'[a-z|A-Z|\s]+,', suburb_temp).group()[:-1]
-                    post_code = re.search(r'[0-9]+', suburb_temp).group()
-                    housetype = house_soup.find('span', attrs={'class':'property-info__property-type'}).text
-                    bedroom = 0
-                    bathroom = 0
-                    parking = 0
-                    feature_str = ''
-                    for feature in house_soup.findAll('li', attrs={'class':'general-features__feature'}):
-                        feature_str = feature_str + feature.get('aria-label')
-                    bed = re.search(r'[0-9]+\sbedroom', feature_str)
-                    if bed is not None:
-                        bedroom = int(re.search(r'[0-9]+', bed.group()).group())
-                    bath = re.search(r'[0-9]+\sbathroom', feature_str)
-                    if bath is not None:
-                        bathroom = int(re.search(r'[0-9]+', bath.group()).group())
-                    park = re.search(r'[0-9]+\sparking space', feature_str)
-                    if park is not None:
-                        parking = int(re.search(r'[0-9]+', park.group()).group())
-                    soldprice = None
-                    soldprice_str = house_soup.find('span', attrs={'class':'property-price property-info__price'}).text
-                    price = re.search(r'[0-9,]+', soldprice_str)
-                    if price is not None:
-                        soldprice = int(price.group().replace(',', ''))
-                    solddate = house_soup.find('p', attrs={'class':'property-info__secondary-content'})
-                    if solddate is not None:
-                        solddate = solddate.text[8:]
-                        try:
-                            solddate = datetime.strptime(solddate, '%d %b %Y')
-                        except:
-                            solddate = None
-                    agent = house_soup.find('p', attrs={'class':'agency-info__name'})
-                    if agent is not None:
-                        agent = agent.text
-                    latitude = re.search(r'"latitude":-[0-9|.]+', house_page)
-                    if latitude is not None:
-                        latitude = -float(re.search(r'[0-9|.]+', latitude.group()).group())
-                    longitude = re.search(r'"longitude":[0-9|.]+', house_page)
-                    if longitude is not None:
-                        longitude = float(re.search(r'[0-9|.]+', longitude.group()).group())
+                    # try to extract house infos from webpage
+                    house_id = house.getHouseID()
+                    address = house.getAddress()
+                    suburb, post_code = house.getSuburb()
+                    housetype = house.getHouseType()
+                    bedroom, bathroom, parking = house.getFeatures()
+                    soldprice = house.getSoldPrice()
+                    solddate = house.getSoldDate()
+                    agency = house.getAgency()
+                    latitude, longitude = house.getLocation()
                     # save to mySQL database
-                    sql = """INSERT INTO house_info_main (
+                    sql = """INSERT INTO victoria_test (
                         house_id, address, suburb, postcode, house_type, bedroom, bathroom,
-                        parking, sold_price, sold_date, agency, latitude, longitude, link) 
-                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+                        parking, sold_price, sold_date, agency, latitude, longitude, link, time) 
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, NOW())"""
                     val = (house_id, address, suburb, post_code, housetype, bedroom, bathroom,
-                        parking, soldprice, solddate, agent, latitude, longitude, house_url)
+                        parking, soldprice, solddate, agency, latitude, longitude, house_url)
                     cursor.execute(sql, val)
                     con.commit()
                     house_count = house_count + 1
