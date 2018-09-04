4b6992a0a49ff2c37bafbdb8d6c1b117e4b527cb
Nan Li <linan.lqq0@gmail.com>
2018-09-04 13:37:55 +1000

    update .sql

--- SQL/TAS.sql
+++ SQL/TAS.sql
@@ -1,2 +1,7 @@
 SELECT * FROM aus_sold_houses.tasmania
 ORDER BY id DESC;
+
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM aus_sold_houses.tasmania
+WHERE postcode >= 8000 OR postcode < 7000;
+SET SQL_SAFE_UPDATES = 1;
--- SQL/WA.sql
+++ SQL/WA.sql
@@ -1,2 +1,7 @@
 SELECT * FROM aus_sold_houses.western_australia
 ORDER BY id DESC;
+
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM aus_sold_houses.western_australia
+WHERE postcode >= 7000 OR postcode < 6000;
+SET SQL_SAFE_UPDATES = 1;
